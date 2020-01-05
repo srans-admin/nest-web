755e41127b607f11e3b6b45a9a1112e13203e2d8
archana <archana@srans.in>
2020-01-05 12:49:44 +0530

    Reverted Unrequired files

---
+++ src/app/_services/notification.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'; 
+import { environment } from '../../environments/environment';
+import { AuthenticationService } from '../_auth/auth.service';
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class NotificationService{
+      private baseUrl = environment.appUrl + '/api/v1/users/{userId}/notifications';
+
+      constructor(private http: HttpClient, 
+                  private authenticationService: AuthenticationService) { }
+
+      getNotification(notification: Array<string>): Observable<any> {
+          return this.http.get(`${this.baseUrl}`, this.authenticationService.getHttpHeaders());
+        }
+
+  }
--- src/app/role/create-role/create-role.component.ts
+++ src/app/role/create-role/create-role.component.ts
@@ -1,5 +1,5 @@
 import { RoleService } from '../../_services/role.service';
-import { Role } from '../../_models/Role';
+import { Role } from '../../_models/role';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
