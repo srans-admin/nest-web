95efdcce149821cd732ad7808b3e334cc4effc60
saikrishna <mskrishna@srans.in>
2020-01-24 23:31:25 +0530

    bed-reservation in adimn dashboard 24Jan2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -65,17 +65,16 @@ import { HostelSubscriptionComponent } from './hostel-subscription/hostel-subscr
 import { ProfileComponent } from './profile/profile.component';
 import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
 
-import { VacateComponent } from './vacate/vacate.component';
-
-import { UserPaymentComponent } from './payment/user-payment/user-payment.component';
+import { BedReservationComponent } from './bed-reservation/bed-reservation.component';
 
 
-
+ 
 
 const routes: Routes = [
   {path: '', component: ProfileComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
+  { path: 'reservation', component: BedReservationComponent},
   { path: 'profile', component: ProfileComponent },
   { path: 'editprofile', component: EditProfileComponent, canActivate: [AuthGuard] },
 
@@ -119,24 +118,22 @@ const routes: Routes = [
   { path: 'invoice/add', component: CreateInvoiceComponent},
 
   { path: 'payment', component: PaymentListComponent, canActivate: [AuthGuard] },
-  { path: 'paymentHistory', component: UserPaymentComponent, canActivate: [AuthGuard] },
   { path: 'payment/add', component: CreatePaymentComponent},
   { path: 'hpayment', component: HostelPaymentComponent, canActivate: [AuthGuard] },
   { path: 'hsubscription', component: HostelSubscriptionComponent, canActivate: [AuthGuard] },
 
   { path: 'complaint', component: ComplaintListComponent, canActivate: [AuthGuard]},
-  { path: 'complaint/add', component: AddComplaintComponent,canActivate: [AuthGuard]},
+  { path: 'complaint/add', component: AddComplaintComponent},
   { path: 'complaintupdate/:id', component: UpdateComplaintComponent},
-  { path: 'complaintdetails/:id', component: ComplaintDetailsComponent},
+  { path: 'compaintdetails/:id', component: ComplaintDetailsComponent},
  
-  { path: 'categories', component: CategoryListComponent },
+ { path: 'categories', component: CategoryListComponent },
   { path: 'categories/add', component: CreateCategoryComponent },
  
   { path: 'cupdate/:id', component: UpdateCategoryComponent },
   { path: 'cdetails/:id', component: CategoryDetailsComponent },
  
-  { path: 'vacateMe', component: VacateComponent, canActivate: [AuthGuard]},
-  
+
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 
